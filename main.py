d88dfe9e105737386c17b2c68e06770f8529fadd
Moses <114920877+Mosespushpa@users.noreply.github.com>
2024-05-16 16:02:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ current_card = {}
 to_learn = {}
 
 try:
-    data = pandas.read_csv("data/words_to_learn.csv")
+    data = pandas.read_csv("words_to_learn.csv")
 except FileNotFoundError:
-    original_data = pandas.read_csv("data/Telugu_to_English.csv")
+    original_data = pandas.read_csv("Telugu_to_English.csv")
     to_learn = original_data.to_dict(orient="records")
 else:
     to_learn = data.to_dict(orient="records")
@@ -44,19 +44,19 @@ win.config(padx=50,pady=50,background=BACKGROUND_COLOR)
 flip_timer = win.after(3000, func=flip_card)
 
 canvas = Canvas(width=800,height=528)
-card_front_img = PhotoImage(file="images/card_front.png")
-card_back_img = PhotoImage(file="images/card_back.png")
+card_front_img = PhotoImage(file="card_front.png")
+card_back_img = PhotoImage(file="card_back.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 45, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0,columnspan=2)
 
-cross_img = PhotoImage(file="images/wrong.png")
+cross_img = PhotoImage(file="wrong.png")
 unknow_button = Button(image=cross_img,highlightthickness=0,command = next_card)
 unknow_button.grid(row=1,column=0)
 
-right_img = PhotoImage(file="images/right.png")
+right_img = PhotoImage(file="right.png")
 know_button = Button(image=right_img,highlightthickness=0,command = is_known)
 know_button.grid(row=1,column=1)
 
